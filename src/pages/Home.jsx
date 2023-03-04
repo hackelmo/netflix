75219203df4d7048901f7a0112508f2dc4dfd2b7
hackelmo <tokkito88@gmail.com>
2023-03-05 04:51:36 +0900

    [FEAT] Create MovieSlide with multi carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Banner from "../components/Banner";
+import MovieSlide from "../components/MovieSlide";
 import { movieAction } from "../redux/actions/movieAction";
 
 export default function Home() {
@@ -16,6 +17,12 @@ export default function Home() {
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <h1>Popular Movie</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>TopRated Movie</h1>
+      <MovieSlide movies={topRatedMovies} />
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upcomingMovies} />
     </div>
   );
 }
